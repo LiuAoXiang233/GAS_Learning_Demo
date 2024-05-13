d5744f87e9b4267b5462f54e97578885ac6b7ff6
unknown <liuaoxiang233@163.com>
2024-05-13 15:43:14 +0800

    Enhanced Input

--- Source/GAS_Learning_Demo/Private/Character/AuraCharacterBase.cpp
+++ Source/GAS_Learning_Demo/Private/Character/AuraCharacterBase.cpp
@@ -7,7 +7,7 @@
 AAuraCharacterBase::AAuraCharacterBase()
 {
  	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = true;
+	PrimaryActorTick.bCanEverTick = false;
 
 	// 创建Weapon组件，并把该组件附加到Character的模型组件下，并忽略其碰撞
 	Weapon = CreateDefaultSubobject<USkeletalMeshComponent>("Weapon");
