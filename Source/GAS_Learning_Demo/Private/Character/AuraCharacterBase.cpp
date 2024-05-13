e05d243a297b0fd24a992ece07c49824a3fb3964
unknown <liuaoxiang233@163.com>
2024-05-13 09:20:02 +0800

    Character BluePrint Setup

--- Source/GAS_Learning_Demo/Private/Character/AuraCharacterBase.cpp
+++ Source/GAS_Learning_Demo/Private/Character/AuraCharacterBase.cpp
@@ -9,6 +9,11 @@ AAuraCharacterBase::AAuraCharacterBase()
  	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	// 创建Weapon组件，并把该组件附加到Character的模型组件下，并忽略其碰撞
+	Weapon = CreateDefaultSubobject<USkeletalMeshComponent>("Weapon");
+	Weapon->SetupAttachment(GetMesh(), FName("WeaponHandSocket"));
+	Weapon->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+	
 }
 
 // Called when the game starts or when spawned
